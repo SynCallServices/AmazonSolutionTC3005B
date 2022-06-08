5932a31a56660788487723e235327bc039e50f82
didimelor <a01023785@itesm.mx>
2022-06-08 16:01:03 -0500

    Solved import conflicts

--- amazon-solution/src/views/Dashboard/Dashboard.js
+++ amazon-solution/src/views/Dashboard/Dashboard.js
@@ -8,7 +8,6 @@ import { Outlet } from 'react-router-dom'
 import AWS from 'aws-sdk';
 import { UserContext } from '../../App.js'
 import ConnectLogIn from './components/ConnectLogIn.js'
-import React, { useState } from "react";
 
 const amazonConnect = new AWS.Connect();
 const docClient = new AWS.DynamoDB.DocumentClient();
