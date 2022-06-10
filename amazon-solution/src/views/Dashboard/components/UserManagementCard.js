67da1b555399455e9b84aecdda16c3c4e5ab9341
Jorge <jorgecabiedes99@hotmail.com>
2022-06-09 19:08:59 -0500

    Switch roles now works

--- amazon-solution/src/views/Dashboard/components/UserManagementCard.js
+++ amazon-solution/src/views/Dashboard/components/UserManagementCard.js
@@ -1,6 +1,57 @@
 import { BsFillTrashFill } from 'react-icons/bs'
+import React from 'react'
+import AWS from 'aws-sdk'
+
+const cognito = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: 'latest',
+  region: process.env.REACT_APP_REGION,
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
+});
+
+const connect = new AWS.Connect({
+  apiVersion: 'latest',
+  region: process.env.REACT_APP_REGION,
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
+});
 
 export default function UserManagementCard(props) {
+
+  const [selectedRole, setSelectedRole] = React.useState(props.role)
+
+  function handleChange(event) {
+    setSelectedRole(event.target.value)
+    switchRole(event.target.value); 
+  }
+
+  async function switchRole (role) {
+    role = process.env[`REACT_APP_${role.toUpperCase()}_ID`]
+    await cognito.adminGetUser({
+        UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+        Username: props.username // MODIFY
+    })
+    .promise()
+    .then(async (data) => {
+        const ConnectId = data.UserAttributes.find((item) => item.Name == "custom:connect_id").Value;
+        await connect.updateUserSecurityProfiles({
+            InstanceId: process.env.REACT_APP_INSTANCE_ID,
+            UserId: ConnectId,
+            SecurityProfileIds: [role]
+        })
+        .promise()
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+  }
+
   return (
     <div className="usercard">
         <div className="usecard-names">
@@ -11,14 +62,14 @@ export default function UserManagementCard(props) {
           <h3 className="usercard-lastname">{props.lastName}</h3>
         </div>
       </div>
-      <p className="usercard-email">{props.email}</p>
-      <p className="usercard-id">{props.agentId}</p>
-      <select name="cars" id="cars">
+      <p className="usercard-email" title={props.email}>{props.email}</p>
+      <p className="usercard-id" title={props.agentId}>{props.agentId}</p>
+      <select value={selectedRole} onChange={handleChange} name="cars" id="cars">
         <option value="agent">Agent</option>
         <option value="supervisor">Supervisor</option>
         <option value="admin">Admin</option>
       </select>
-      <BsFillTrashFill />
+      <BsFillTrashFill className='delete-user'/>
     </div>
   )
 
